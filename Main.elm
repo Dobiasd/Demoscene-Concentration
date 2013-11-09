085ca6cbe01c6d7bb9624a2b248c8a33149f513b
Dobiasd <harry@daiw.de>
2013-11-09 16:36:52 +0100

    game logic

--- Main.elm
+++ Main.elm
@@ -17,22 +17,18 @@ import Window
 -- \---------------------/
 
 {-| The game field extends from -100 to +100 in x and y coordinates. -}
-(gameWidth,gameHeight) = (1000,1000) -- todo (200,200)
+(gameWidth,gameHeight) = (200,200)
 framesPerSecond = 60
 effects : [Effect]
-effects = [plasma, starfield]
+effects = [effect "plasma" plasma, effect "starfield" starfield]
 
 
 -- /--------------------\
 -- | view configuration |
 -- \--------------------/
 
-manualText = "Guide the ball safely to its goal (green)."
-respawnText = "Please go to the start (yellow) to begin/respawn."
 timeTextHeight = 7
 timeTextPosY = 95
-textHeight = 5
-textPosY = -90
 
 
 -- /--------\
@@ -44,6 +40,8 @@ data Action = Tap Point | Step Float
 type Input = { winSize:(Int,Int), action:Action }
 
 speed : Signal Time
+--speed = lift ((/)1000) <| fps framesPerSecond
+--speed = lift inSeconds <| fps framesPerSecond
 speed = fps framesPerSecond
 
 input : Signal Input
@@ -66,6 +64,7 @@ actions = merge steps flips
 -- | model |
 -- \-------/
 
+type Named      a = { a | name:String }
 type Positioned a = { a | x:Float, y:Float }
 type Sized      a = { a | w:Float, h:Float }
 type Boxed      a = Sized (Positioned a)
@@ -73,10 +72,13 @@ type Boxed      a = Sized (Positioned a)
 type Point = Positioned {}
 type Box = Boxed {}
 
-type Effect = (Time -> Form)
-type Card = Boxed ({effect:Effect, status:CardStatus})
+type EffectFunc = Time -> Form
+type Effect = Named {func:EffectFunc}
+type Card = Boxed {effect:Effect, status:CardStatus}
 type Cards = [Card]
 
+effect : String -> EffectFunc -> Effect
+effect name func = {name=name, func=func}
 
 -- todo move effects to own modules
 plasma : Time -> Form
@@ -85,23 +87,29 @@ plasma t = rect 200 200 |> filled (rgb 255 0 0)
 starfield : Time -> Form
 starfield t = rect 200 200 |> filled (rgb 0 0 255)
 
+backside : Time -> Form
+backside _ = rect 200 200 |> filled (rgb 0 0 0)
+
+doneOverlay : Time -> Form
+doneOverlay _ = rect 200 200 |> filled (rgba 0 0 0 0.5)
+
 
 {-| Creation of one single row of cards with equidistant gaps. -}
 cardBoxRow : Float -> [Box]
 cardBoxRow y =
   let
     cols = 2
-    distX = 50 -- todo calculate dynamically
+    distX = 90 -- todo calculate dynamically
     xOff = toFloat (ceiling  (-cols / 2)) * distX / 2
-    cardWidth = 40
-    cardHeight = 40
+    cardWidth = 70
+    cardHeight = 70
   in
     map (\x -> box (distX * x + xOff) y cardWidth cardHeight) [0..cols-1]
 
 cardBoxes =
   let
     rows = 2
-    distY = 50 -- todo calculate dynamically
+    distY = 90 -- todo calculate dynamically
     yOff = toFloat (ceiling  (-rows / 2)) * distY / 2
   in
     map (((+) yOff ) . (*) distY) [0..rows-1] |> map cardBoxRow |> concat
@@ -110,8 +118,14 @@ card effect box =
   let boxedEffect = { box | effect=effect }
   in { boxedEffect | status=FaceDown }
 
+
+
 -- todo: shuffle effects
-cards = map (card starfield) cardBoxes
+e1 = head effects
+e2 = (head . tail) effects
+cards : Cards
+cards = zipWith (\effect box -> card effect box) [e1, e1, e2, e2] cardBoxes
+
 
 
 data State = Start | Play | Won
@@ -140,16 +154,16 @@ defaultGame =
 {-| Since the game is always scaled maximally into the window
 (keeping its aspect ratio), the mouse and touch positions
 have to be converted to game positions. -}
-winPosToGamePos : (Int,Int) -> (Int,Int) -> (Float, Float)
+winPosToGamePos : Positioned a -> (Int,Int) -> Point
 winPosToGamePos pos size =
   let
     intPairToFloatPair (a, b) = (toFloat a, toFloat b)
-    (winX, winY) = intPairToFloatPair pos
+    (winX, winY) = (pos.x, pos.y)
     (sizeX, sizeY) = intPairToFloatPair size
     (middleX, middleY) = (sizeX / 2, sizeY / 2)
     factor = gameScale size (gameWidth,gameHeight)
   in
-    ((winX - middleX) / factor, (middleY - winY) / factor)
+    point ((winX - middleX) / factor) ((middleY - winY) / factor)
 
 {-| Calculate factor by which the game is scaled visually onto the screen. -}
 gameScale : (Int,Int) -> (Float,Float) -> Float
@@ -157,7 +171,7 @@ gameScale (winW, winH) (gameW,gameH) =
   min (toFloat winW / gameW) (toFloat winH / gameH)
 
 gameState : Signal Game
-gameState = foldp stepGame defaultGame actions
+gameState = foldp stepGame defaultGame input
 
 stepDelta : Time -> Game -> Game
 stepDelta delta ({state, time} as game) =
@@ -165,29 +179,106 @@ stepDelta delta ({state, time} as game) =
     Play -> { game | time <- if state == Start then time else time + delta }
     _ -> game
 
-stepTap : Point -> Game -> Game
-stepTap tapPos game = { game | state <- Play }
+inBox : Positioned a -> Boxed b -> Bool
+inBox pos box =
+  let
+    xDist = abs (pos.x - box.x)
+    yDist = abs (pos.y - box.y)
+  in
+    xDist <= box.w/2 && yDist <= box.h/2
 
-stepGame : Action -> Game -> Game
-stepGame action ({state, time} as game) =
+goflipCardsHit : Point -> Card -> Cards -> Cards
+goflipCardsHit tapPos ({status} as card) acc =
+  let
+    hit = tapPos `inBox` card
+    status' = if not hit then status else case status of
+                                            Done -> Done
+                                            FaceUp -> FaceDown
+                                            FaceDown -> FaceUp
+    card' = { card | status <- status' }
+  in
+    card'::acc
+
+flipCardsHit : Point -> Cards -> Cards
+flipCardsHit tapPos cards =
+    foldr (goflipCardsHit tapPos) [] cards
+
+allEqual : [a] -> Bool
+allEqual xs = if length xs < 2 then True
+                               else all ((==) (head xs)) (tail xs)
+
+-- todo: simplify
+stepTap : Point -> Game -> Game
+stepTap gameTapPos ({state,cards} as game) =
+  let
+    (cardsDone, cardsNotDone) = partition (((==) Done) . (.status)) cards
+    cardsNotDoneFaceDown = map (\c -> {c | status <- FaceDown}) cardsNotDone
+    faceUpCount = cards |> filter (((==) FaceUp) . (.status)) |> length
+    cardsNotDone' = flipCardsHit gameTapPos <|
+                      if faceUpCount == 2 then cardsNotDoneFaceDown
+                                          else cardsNotDone
+    (cardsNotDoneUp', cardsNotDoneDown') =
+       partition (((==) FaceUp) . (.status)) cardsNotDone'
+    cardsNotDoneUpEffectNames = map (.name . .effect) cardsNotDoneUp'
+    names = cardsNotDoneUpEffectNames
+    foundPair = length names == 2 && allEqual names
+    cardsNotDoneUpDone' = map (\c -> {c | status <- Done}) cardsNotDoneUp'
+    allDone = all
+    cards' = cardsDone ++ cardsNotDoneDown'
+             ++ if foundPair then cardsNotDoneUpDone' else cardsNotDoneUp'
+    state' = case state of
+               Won -> Won
+               Start -> Play
+               Play -> if all (((==) Done) . .status) cards' then Won
+                                                             else Play
+  in
+    { game | state <- state',
+             cards <- cards' }
+
+stepGame : Input -> Game -> Game
+stepGame ({action,winSize}) ({state, time} as game) =
   case action of
-    Step delta -> { game | time <- if state == Start
-      then time
-      else time + delta }
-    Tap tapPos -> stepTap tapPos game
+    Step delta -> { game | time <- case state of
+                                     Play -> time + delta
+                                     _ -> time }
+    Tap tapPos -> stepTap (winPosToGamePos tapPos winSize) game
 
 
 -- /---------\
 -- | display |
 -- \---------/
 
+displayCard : Time -> Card -> Form
+displayCard time card =
+  let texture = case card.status of
+                  FaceDown -> backside time
+                  FaceUp -> card.effect.func time
+                  Done -> group [card.effect.func time, doneOverlay time]
+  in texture |> move (card.x, card.y) |> scale (card.w / 200)
+
+
+displayCards : Time -> Cards -> Form
+displayCards time cards =
+  map (displayCard time) cards |> group
+
+{-| Render text using a given transformation function. -}
+txt : (Text -> Text) -> String -> Element
+txt f = text . f . monospace . Text.color lightBlue . toText
+
 {-| Draw game into a form with size (gameWidth,gameHeight). -}
 display : Game -> Form
-display game =
-  group
-    [
-      asText game |> toForm
-    ]
+display ({state,time,cards} as game) =
+  let
+    timeTextForm = txt (Text.height timeTextHeight) (show <| time / 1000)
+                     |> toForm |> move (0, timeTextPosY)
+  in
+    group
+      [
+        displayCards time cards
+        , timeTextForm
+        , asText game |> toForm |> scale 0.2
+        --, asText cards |> toForm |> scale 0.2
+      ]
 
 {-| Draw game maximized into the window. -}
 displayFullScreen : (Int,Int) -> Game -> Element
